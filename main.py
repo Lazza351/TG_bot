c7cd2633e84a7942869be14bf99fd2a14784a0f4
Lazza351 <124282431+Lazza351@users.noreply.github.com>
2023-11-14 20:16:49 +0300

    test

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ bot = AsyncTeleBot('6147456112:AAERBvts7QYb9Fzy1u1vSpzPYGJC5IdQ7gk')
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
 
-    await bot.send_message( message.chat.id, 'Привет, я бот для проверки телеграмм webapps!)', reply_markup=webAppKeyboard()) 
+    await bot.send_message(message.chat.id, 'Привет, я бот для проверки телеграмм webapps!)', reply_markup=inlinebutton())
 
-    
+
+def inlinebutton():
+    markup = types.InlineKeyboardMarkup()
+    webAppTest = types.WebAppInfo("https://lazza351.github.io/TG_bot/")
+    button = types.InlineKeyboardButton("Приложение", web_app=webAppTest)
+    markup.add(button)
+
+    return markup
+
 def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
-   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
+   keyboard = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True) #создаем клавиатуру
    webAppTest = types.WebAppInfo("https://lazza351.github.io/TG_bot/") #создаем webappinfo - формат хранения url
-   one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
+   one_butt = types.KeyboardButton(text="Открыть приложение", web_app=webAppTest) #создаем кнопку типа webapp
    keyboard.add(one_butt) #добавляем кнопки в клавиатуру
 
    return keyboard
